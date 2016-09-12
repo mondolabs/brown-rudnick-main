82ff6e421bd426c713a7995e36e439e3ade7bf84
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-12 17:59:56 -0400

    top content

--- wp-content/themes/brown-rudnick-main/page-templates/alerts.php
+++ wp-content/themes/brown-rudnick-main/page-templates/alerts.php
@@ -8,16 +8,14 @@ $post = new TimberPost();
 $data['post'] = $post;
 $data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
 $data['featured_image_url'] = $data['featured_image_url'][0];
-
 $data['insights_header_text'] = get_field('insights_header_text');
 $data['insights_sidebar_header'] = get_field('insights_sidebar_header');
 $data['insights_sidebar_items'] = get_field('insights_sidebar_items');
-
 $data['contact_name'] = get_field('contact_name');
 $data['contact_title'] = get_field('contact_title');
 $data['contact_phone_number'] = get_field('contact_phone_number');
 $data['contact_email'] = get_field('contact_email');
-
+$data['top_content'] = get_field('top_content');
 $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
 
 ?>
