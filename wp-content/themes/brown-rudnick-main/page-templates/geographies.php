2c5d414d83205dc897a71da9d8091039778d54dd
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-13 12:10:49 -0400

    refactoring views - insight landing

--- wp-content/themes/brown-rudnick-main/page-templates/geographies.php
+++ wp-content/themes/brown-rudnick-main/page-templates/geographies.php
@@ -14,10 +14,12 @@ $data['geographies'] = Timber::get_posts($geographies_posts_args);
 $data['post'] = $post;
 $data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
 $data['featured_image_url'] = $data['featured_image_url'][0];
-$data['geographies_header_text'] = get_field('geographies_header_text');
-$data['geographies_sidebar_header'] = get_field('geographies_sidebar_header');
-$data['geographies_sidebar_items'] = get_field('geographies_sidebar_items');
+$data['header_text'] = get_field('header_text');
+$data['sidebar_header'] = get_field('sidebar_header');
+$data['sidebar_items'] = get_field('sidebar_items');
 $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
+$slug = basename(get_permalink());
+$data['slug'] = $slug;
 ?>
 
 <html>
@@ -26,7 +28,7 @@ $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arro
   </head>
   <body>
     <div id="page-full-width-homepage" class ="full-width" role="main">
-      <?php Timber::render('/twig-templates/geographies.twig', $data); ?>
+      <?php Timber::render('/twig-templates/experience_landing.twig', $data); ?>
     </div>  
     <?php do_action( 'foundationpress_after_content' ); ?>
     <?php get_footer(); ?>
--- wp-content/themes/brown-rudnick-main/page-templates/practices.php
+++ wp-content/themes/brown-rudnick-main/page-templates/practices.php
@@ -18,7 +18,8 @@ $data['header_text'] = get_field('header_text');
 $data['sidebar_header'] = get_field('sidebar_header');
 $data['sidebar_items'] = get_field('sidebar_items');
 $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
-
+$slug = basename(get_permalink());
+$data['slug'] = $slug;
 ?>
 
 <html>
