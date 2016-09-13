2b69749d5dc6a66037257e5089fa40477382eca6
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-13 11:01:12 -0400

    refactoring views - insight landing

--- wp-content/themes/brown-rudnick-main/page-templates/practices.php
+++ wp-content/themes/brown-rudnick-main/page-templates/practices.php
@@ -27,7 +27,7 @@ $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arro
   </head>
   <body>
     <div id="page-full-width-homepage" class ="full-width" role="main">
-      <?php Timber::render('/twig-templates/practices.twig', $data); ?>
+      <?php Timber::render('/twig-templates/experience_landing.twig', $data); ?>
     </div>  
     <?php do_action( 'foundationpress_after_content' ); ?>
     <?php get_footer(); ?>
