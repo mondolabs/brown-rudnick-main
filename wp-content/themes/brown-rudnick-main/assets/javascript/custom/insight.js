75c527c9637c0901d41765fbb20a8e237e2f450c
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-14 10:19:55 -0400

    insights filters js

--- wp-content/themes/brown-rudnick-main/assets/javascript/custom/insight.js
+++ wp-content/themes/brown-rudnick-main/assets/javascript/custom/insight.js
@@ -2,18 +2,28 @@ var INSIGHTS = {
 	listeners: function(){
 		console.log('Insights listeners js loaded');
 		$('select').change(function(event) {
-			var filters = $('select');
-			var queryString = location.href;
+			var selects = $('select');
+			var queryStringBase = location.origin + location.pathname;
+			var queryString = "";
+			var filters = [];
+			for (var i = selects.length - 1; i >= 0; i--) {
+				var select = selects[i];
+				if ( $(select).val().length > 0 ) {
+					filters.push($(select));
+				}
+			}
+			console.log(filters);
 			for(var i = filters.length - 1; i >= 0; i--) {
 				var paramName = $(filters[i]).attr('name');
 				var paramValue = $(filters[i]).val();
-				if ( paramValue.length > 0 && i === 0 ) {
-					queryString = queryString + "?" + paramName + "=" + paramValue;
+				if ( paramValue.length > 0 && i === filters.length - 1 ) {
+					queryString = queryStringBase + "?" + paramName + "=" + paramValue;
+					console.log(queryString);
 				} else if ( paramValue.length > 0 ) {
 					queryString = queryString + "&" + paramName + "=" + paramValue;
+					console.log(queryString);
 				}
 			}
-
 			window.location.replace(queryString);
 		});
 	},
@@ -29,7 +39,6 @@ var INSIGHTS = {
 		geographySelect.val(decodeURIComponent(selectedGeography));
 		industrySelect.val(decodeURIComponent(selectedIndustry));
 		practiceSelect.val(decodeURIComponent(selectedPractice));
-
 	}
 }
 
