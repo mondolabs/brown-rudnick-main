4f0c76ee7473cd49fd3a55c8e3b45bd4aab5ba59
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-19 10:38:16 -0400

    people template set up

---
+++ wp-content/themes/brown-rudnick-main/page-templates/people.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Template Name: People
+*/
+get_header();
+$data = Timber::get_context();
+$practice_posts_args = array(
+    'post_type' =>  'practice',
+    'orderby' => 'title',
+    'order' => 'ASC',
+    'posts_per_page'=>-1
+);
+$data['experiences'] = Timber::get_posts($practice_posts_args);
+$data['post'] = $post;
+$data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
+$data['featured_image_url'] = $data['featured_image_url'][0];
+$data['header_text'] = get_field('header_text');
+$data['sidebar_header'] = get_field('sidebar_header');
+$data['sidebar_items'] = get_field('sidebar_items');
+$data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
+
+
+
+?>
+
+<html>
+  <head>
+    <?php wp_head()?>
+  </head>
+  <body>
+    <div id="page-full-width-homepage" class ="full-width" role="main">
+      <?php Timber::render('/twig-templates/people.twig', $data); ?>
+    </div>  
+    <?php do_action( 'foundationpress_after_content' ); ?>
+    <?php get_footer(); ?>
+  </body>
+</html>
