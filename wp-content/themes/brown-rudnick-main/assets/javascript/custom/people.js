8749bbb9bd3c33b5a51c9eaaa910dcf63fada9a4
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-20 15:01:10 -0400

    all-people page

--- wp-content/themes/brown-rudnick-main/assets/javascript/custom/people.js
+++ wp-content/themes/brown-rudnick-main/assets/javascript/custom/people.js
@@ -1,6 +1,15 @@
 var PEOPLE = {
 	listeners: function(){
 		console.log('People js loaded');
+		$('.letter__link').click(function(event) {
+			var letter = $(this).data('letter');
+			var letterAnchor = $("div[data-letter-anchor="+ letter +"]");
+			$('html,body').animate({
+	          scrollTop: letterAnchor.offset().top - 200
+	        }, 1000);
+	        console.log("Scrolling to" + letter )
+	        return false;
+		});
 	}
 }
 
--- wp-content/themes/brown-rudnick-main/page-templates/people.php
+++ wp-content/themes/brown-rudnick-main/page-templates/people.php
@@ -20,9 +20,7 @@ $data['sidebar_items'] = get_field('sidebar_items');
 $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
 $data['alphabet'] = range('A', 'Z');
 
-
-
-?> 
+?>
 
 <html>
   <head>
