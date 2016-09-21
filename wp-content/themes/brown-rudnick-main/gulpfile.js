688896d93c54e5b6064e797473cf4a7605ce4a66
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-21 13:14:30 -0400

    single people page

--- wp-content/themes/brown-rudnick-main/gulpfile.js
+++ wp-content/themes/brown-rudnick-main/gulpfile.js
@@ -290,3 +290,15 @@ gulp.task('default', ['build', 'browser-sync'], function() {
       logFileChange(event);
     });
 });
+
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
