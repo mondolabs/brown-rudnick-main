62722c98a1e53a35dac11f009517f81b6675a3bd
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-27 18:01:27 -0400

    locations page

--- wp-content/themes/brown-rudnick-main/page-templates/locations.php
+++ wp-content/themes/brown-rudnick-main/page-templates/locations.php
@@ -12,9 +12,15 @@ $data['banner_image'] = get_field('banner_image');
 $data['bottom_banner_image'] = get_field('bottom_banner_image');
 $data['bottom_banner_header_text'] = get_field('bottom_banner_header_text');
 $data['bottom_banner_content_text'] = get_field('bottom_banner_content_text');
-$data['locations'] = get_field('locations');
+$locations_args = 
+$locations_args = array(
+    'post_type' =>  'location',
+    'orderby' => 'title',
+    'order' => 'ASC',
+    'posts_per_page'=>-1
+);
+$data['locations'] = Timber::get_posts($locations_args);
 $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
-
 
 ?>
 
