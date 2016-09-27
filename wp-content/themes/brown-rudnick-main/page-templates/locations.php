d36c82c0bccc6c1b0a955946e886c3ea3a28e237
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-09-27 14:34:50 -0400

    all locations page setup

---
+++ wp-content/themes/brown-rudnick-main/page-templates/locations.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: All Locations Page
+*/
+get_header();
+$data = Timber::get_context();
+$post = new TimberPost();
+$data['post'] = $post;
+$data['banner_image'] = get_field('banner_image');
+$data['locations_header_text'] = get_field('locations_header_text');
+$data['bottom_banner_image'] = get_field('bottom_banner_image');
+$data['bottom_banner_header_text'] = get_field('bottom_banner_header_text');
+$data['bottom_banner_content_text'] = get_field('bottom_banner_content_text');
+$data['locations'] = get_field('locations');
+$data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
+$data['featured_image_url'] = $data['featured_image_url'][0];
+$data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
+?>
+
+<html>
+  <head>
+    <?php wp_head()?>
+  </head>
+  <body>
+    <div id="page-full-width-homepage" class ="full-width" role="main">
+      <?php Timber::render('/twig-templates/locations.twig', $data); ?>
+    </div>  
+    <?php do_action( 'foundationpress_after_content' ); ?>
+    <?php get_footer(); ?>
+  </body>
+</html>
+
+
+
---
+++ wp-content/themes/brown-rudnick-main/single-location.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Single Location Page
+*/
+
+get_header();
+
+$data = Timber::get_context();
+$data['location'] = new TimberPost();
+$data['specialization'] = get_field('specialization');
+$data['related_experiences'] = get_field('related_experiences');
+
+$data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
+$data['featured_image_url'] = $data['featured_image_url'][0];
+
+?>
+<html>
+  <head>
+    <?php wp_head()?>
+  </head>
+  <body>
+    <div id="page-full-width-homepage" class ="full-width" role="main">
+      <?php Timber::render('/twig-templates/person.twig', $data); ?>
+    </div>  
+    <?php do_action( 'foundationpress_after_content' ); ?>
+    <?php get_footer(); ?>
+  </body>
+</html>
