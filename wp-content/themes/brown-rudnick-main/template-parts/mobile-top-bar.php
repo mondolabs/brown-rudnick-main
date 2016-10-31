b3bd483da3b8db73184864b5d19cee794aff123e
Luis Castaneda <luiscastaneda@Luiss-MacBook-Pro.local>
2016-10-31 09:42:04 -0400

    mobile menu with right assets and links

--- wp-content/themes/brown-rudnick-main/template-parts/mobile-top-bar.php
+++ wp-content/themes/brown-rudnick-main/template-parts/mobile-top-bar.php
@@ -12,12 +12,16 @@
   <?php foundationpress_mobile_nav(); ?>
   <div class="mobile__menu__bottom">
   	<div class="one__half padding__wrapper five">
-  		<div class="contact__us__mobile"></div>
-  		<p>Contact us</p>
+      <a href="/contact-us/">
+    		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42.37 39.53"><defs><style>.cls-1{fill:none;stroke:#221f1f;stroke-linejoin:round;stroke-width:5px;}</style></defs><title>Asset 1</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M39.87,17.75C39.87,26.17,31.51,33,21.19,33a22.43,22.43,0,0,1-6.55-1L3.9,37l4.21-8.39A14,14,0,0,1,2.5,17.75C2.5,9.33,10.87,2.5,21.19,2.5S39.87,9.33,39.87,17.75Z"/></g></g></svg>
+    		<p>Contact us</p>
+      </a>
   	</div>
   	<div class="one__half padding__wrapper five">
-  		<div class="locations__mobile"></div>
-  		<p>Locations</p>
+      <a href="/locations/">
+    		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.75 55.13"><defs><style>.cls-1{fill:none;stroke:#b91628 !important; stroke-linecap:round;stroke-linejoin:round;stroke-width:7px;}.cls-2{fill:#231f20; stroke:#b91628 !important;}</style></defs><title>Asset 2</title><g id="Layer_2" data-name="Layer 2"><g id="icons"><path class="cls-1" d="M36.25,19.19c0,8.67-16.37,32.43-16.37,32.43S3.5,27.86,3.5,19.19,10.83,3.5,19.88,3.5,36.25,10.53,36.25,19.19Z"/><circle class="cls-2" cx="20.09" cy="18.72" r="5.91"/></g></g></svg>
+    		<p>Locations</p>
+      </a>
   	</div>
   </div>
 </nav>
