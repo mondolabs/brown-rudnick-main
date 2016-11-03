b01b6b366a9b837599209190f8f254b2a048bb17
mondolabs <development@mondo.com>
2016-11-03 09:39:10 -0400

    message from ceo file name

--- wp-content/themes/brown-rudnick-main/page-templates/message-from-the-ceo.php
+++ wp-content/themes/brown-rudnick-main/page-templates/message-from-the-ceo.php
@@ -8,8 +8,14 @@ $post = new TimberPost();
 $data['post'] = $post;
 $data['banner_image'] = get_field('banner_image');
 $data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
-$data['about_sidebar_header'] = get_field('about_sidebar_header');
-$data['about_sidebar_items'] = get_field('about_sidebar_items');
+$sidebar_slug = 'about-sidebar';
+$args = array(
+  'name'        => $sidebar_slug,
+  'post_type'   => 'sidebar',
+  'post_status' => 'publish',
+  'numberposts' => 1
+);
+$data['sidebar'] = get_posts($args);
 $data['ceo_header_text'] = get_field('ceo_header_text');
 $data['ceo_image'] = get_field('ceo_image');
 $data['ceo_name'] = get_field('ceo_name');
