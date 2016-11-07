d52f859545518ad74b8c6bed42a8d21762114ed6
Luis Castaneda <luiscastaneda@Luiss-MacBook-Pro.local>
2016-11-07 10:35:41 -0500

    navbar

--- wp-content/themes/brown-rudnick-main/assets/javascript/custom/nav.js
+++ wp-content/themes/brown-rudnick-main/assets/javascript/custom/nav.js
@@ -37,6 +37,7 @@ var NAV = {
 		var mobileCheck = $(window).scrollTop() > 250;
 		var check;
 		if ($(document).width() >= 640) {
+			console.log($(document).width());
 			check = desktopCheck;
 		} else {
 			check = mobileCheck;
