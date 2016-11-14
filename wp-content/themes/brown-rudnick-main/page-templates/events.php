bd56501022a1c30abb1837c77bcd7add0c50015e
mondolabs <development@mondo.com>
2016-11-14 15:44:54 -0500

    events page + individual event

--- wp-content/themes/brown-rudnick-main/page-templates/events.php
+++ wp-content/themes/brown-rudnick-main/page-templates/events.php
@@ -24,9 +24,7 @@ $data['contact_name'] = get_field('contact_name');
 $data['contact_title'] = get_field('contact_title');
 $data['contact_phone_number'] = get_field('contact_phone_number');
 $data['contact_email'] = get_field('contact_email');
-$data['top_content'] = get_field('top_content');
-$data['hover_arrow'] = get_template_directory_uri() . "/assets/images/hover-arrow.png";
-$slug = basename(get_permalink());
+
 $data['slug'] = $slug;
 
 $data['upcoming_events'] = get_field('upcoming_events');
@@ -38,6 +36,31 @@ $post_type_args = array(
   'posts_per_page' => -1
 );
 
+$date = get_query_var('date_query', "");
+
+$year = substr($date, -4 );
+$month = substr($date, 0, 2 );
+$posts_args = array(
+  'post_type' => 'event',
+  'numberposts' => -1,
+  'posts_per_page' => -1,
+  'orderby' => 'date',
+);
+
+$data['posts_collection'] = get_posts($posts_args);
+$posts_from_collection_args = array(
+  'post_type' => 'event',
+  'numberposts' => -1,
+  'posts_per_page' => -1,
+  'orderby' => 'date',
+);
+
+$dates = [];
+$posts_from_collection = get_posts($posts_from_collection_args);
+foreach ( $posts_from_collection as $post_from_collection ) {
+  array_push( $dates, strtotime($post_from_collection->post_date));
+};
+$data['dates'] = array_unique($dates);
 $custom_posts = get_posts($post_type_args);
 $ids = [];
 
@@ -49,17 +72,69 @@ $data['geographies'] = wp_get_object_terms( $ids, 'geography' );
 $data['industries'] = wp_get_object_terms( $ids, 'industry' );
 $data['practices'] = wp_get_object_terms( $ids, 'practice' );
 
+$date_query_term = get_query_var('date_query', "DATE");
 $geography = get_query_var('geography_query', "GEOGRAPHIES");
 $industry = get_query_var('industry_query', "INDUSTRIES");
 $practice = get_query_var('practice_query', "PRACTICES");
 
-$geography =  str_replace("-and-", " & ", $geography);
-$industry =  str_replace("-and-", " & ", $industry);
-$practice =  str_replace("-and-", " & ", $practice);
+if (($geography !== "GEOGRAPHIES") || ( $industry !== "INDUSTRIES") || ($practice !=="PRACTICES") || $date_query_term !== "DATE" ) {
 
-$geography =  str_replace("-slash-", " / ", $geography);
-$industry =  str_replace("-slash-", " / ", $industry);
-$practice =  str_replace("-slash-", " / ", $practice);
+  $geography =  str_replace("-and-", " & ", $geography);
+  $industry =  str_replace("-and-", " & ", $industry);
+  $practice =  str_replace("-and-", " & ", $practice);
+
+  $geography =  str_replace("-slash-", " / ", $geography);
+  $industry =  str_replace("-slash-", " / ", $industry);
+  $practice =  str_replace("-slash-", " / ", $practice);
+
+
+  $year_query = intval($year);
+  $month_query = intval($month); 
+  $events_args = array(
+    'post_type' => 'event',
+    'posts_per_page' => -1, 
+    'orderby'=>'date',
+    'date_query' => array(
+      array(
+        'column' => "date",
+        'year'  => $year_query,
+        'month' => $month_query,
+      ),
+    )
+  );
+
+  if( ($geography !== "GEOGRAPHIES") || ( $industry !== "INDUSTRIES") || ($practice !=="PRACTICES")  ) {
+    $events_args["tax_query"] = array( 'relation' => 'AND' );
+    if ( $geography !== "GEOGRAPHIES" ) {
+      $geography_term_query_array = array('taxonomy' => 'geography', 'field' => 'slug', 'terms' => array( $geography));
+      array_push($events_args['tax_query'], $geography_term_query_array );
+    }
+    if ( $industry !== "INDUSTRIES" ) {
+      $industry_term_query_array = array('taxonomy' => 'industry', 'field' => 'slug', 'terms' => array( $industry));
+      array_push($events_args['tax_query'], $industry_term_query_array );
+    }
+    if ( $practice !== "PRACTICES" ) {
+      $practice_term_query_array = array('taxonomy' => 'practice', 'field' => 'slug', 'terms' => array( $practice));
+      array_push($events_args['tax_query'], $practice_term_query_array );
+    }
+  }
+
+  $results = Timber::get_posts($events_args);
+  $data['events'] = $results;
+  $data['events'] = array_unique($data['events']);
+
+  function sort_objects_by_date($a, $b) {
+    if($a->date == $b->date){
+      return 0;
+    }
+    return ($a->date < $b->date) ? -1 : 1;
+  }
+
+  usort($data['events'], "sort_objects_by_date");
+  $data['events'] = array_slice($data['events'], 0, 5 );
+  $data['events'] = array_reverse($data['events']);
+
+}
 
 ?>
 
