0cb588ea7bc5fe13e67141db3dd0cbd3b96c8876
mondolabs <development@mondo.com>
2016-11-14 12:05:21 -0500

    single event page

--- wp-content/themes/brown-rudnick-main/page-templates/events.php
+++ wp-content/themes/brown-rudnick-main/page-templates/events.php
@@ -6,10 +6,20 @@ Template Name: Events
 get_header();
 
 $data = Timber::get_context();
+$post = new TimberPost();
+$data['post'] = $post;
 $data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
 $data['featured_image_url'] = $data['featured_image_url'][0];
-$data['sidebar_header'] = get_field('sidebar_header');
-$data['sidebar_items'] = get_field('sidebar_items');
+$parent = get_page($post->post_parent);
+$parent_name = $parent->post_name;
+$sidebar_slug = $parent_name . '-sidebar';
+$args = array(
+  'name'        => $sidebar_slug,
+  'post_type'   => 'sidebar',
+  'post_status' => 'publish',
+  'numberposts' => 1
+);
+$data['sidebar'] = get_posts($args);
 $data['contact_name'] = get_field('contact_name');
 $data['contact_title'] = get_field('contact_title');
 $data['contact_phone_number'] = get_field('contact_phone_number');
---
+++ wp-content/themes/brown-rudnick-main/single-event.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Single Event Page
+*/
+get_header();
+
+$data = Timber::get_context();
+$data['post'] = new TimberPost();
+$data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
+$data['featured_image_url'] = $data['featured_image_url'][0];
+$data['related_experiences'] = get_field('related_experiences');
+$data['parent_page_name'] = "Events";
+$data['parent_page_url'] = "/events";
+
+?>
+<html>
+  <head>
+    <?php wp_head()?>
+  </head>
+  <body>
+    <div id="page-full-width-homepage" class ="full-width" role="main">
+      <?php Timber::render('/twig-templates/event.twig', $data); ?>
+    </div>  
+    <?php do_action( 'foundationpress_after_content' ); ?>
+    <?php get_footer(); ?>
+  </body>
+</html>
