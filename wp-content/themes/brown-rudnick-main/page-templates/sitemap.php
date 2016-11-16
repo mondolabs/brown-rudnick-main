1c02aab30d9a45156eda78ddf6d73a1df686193a
Luis Castaneda <luiscastaneda@Luiss-MacBook-Pro.local>
2016-11-16 15:40:22 -0500

    new templates

---
+++ wp-content/themes/brown-rudnick-main/page-templates/sitemap.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Sitemap Page
+*/
+get_header();
+$data = Timber::get_context();
+
+?>
+
+<html>
+  <head>
+    <?php wp_head()?>
+  </head>
+  <body>
+    <div id="page-full-width-homepage" class ="full-width" role="main">
+      <?php Timber::render('/twig-templates/sitemap.twig', $data); ?>
+    </div>  
+    <?php do_action( 'foundationpress_after_content' ); ?>
+    <?php get_footer(); ?>
+  </body>
+</html>
