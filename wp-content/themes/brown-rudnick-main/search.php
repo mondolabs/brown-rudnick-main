bb07d3d33bd651b84e591cdf26e2f3db0f9e10fa
Luis Castaneda <luiscastaneda@Luiss-MacBook-Pro.local>
2016-11-22 17:53:35 -0500

    search pagination

--- wp-content/themes/brown-rudnick-main/search.php
+++ wp-content/themes/brown-rudnick-main/search.php
@@ -14,6 +14,11 @@ $search_args = array(
     's' =>  $search_query,
     'posts_per_page'=> -1
 );
+
+
+
+
+
 $data['results'] = Timber::get_posts($search_args);
 $data['search_banner_image'] = get_template_directory_uri() . "/assets/images/search-bg.png";
 $data['form_action'] = get_home_url();
@@ -28,6 +33,31 @@ $data['search_query'] = $search_query;
   <body>
     <div id="page-full-width-homepage" class ="full-width" role="main">
       <?php Timber::render('/twig-templates/search-results.twig', $data); ?>
+      
+
+
+  <div class ="results__container">
+    <?php if ( have_posts() ) : ?>
+      <?php while ( have_posts() ) : the_post(); ?>
+          <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+      <?php endwhile; ?>
+      <?php else : ?>
+        <?php get_template_part( 'template-parts/content', 'none' ); ?>
+    <?php endif;?>
+
+    <?php do_action( 'foundationpress_before_pagination' ); ?>
+
+    <?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+
+      <nav id="post-nav">
+        <div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+        <div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+      </nav>
+    <?php } ?>
+
+</div>
+
+
     </div>  
     <?php do_action( 'foundationpress_after_content' ); ?>
     <?php get_footer(); ?>
