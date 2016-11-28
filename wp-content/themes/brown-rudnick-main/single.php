498bd35e596dca8683e5ad8e162177730ef711f5
mondolabs <development@mondo.com>
2016-11-28 15:59:54 -0500

    comments

--- wp-content/themes/brown-rudnick-main/single.php
+++ wp-content/themes/brown-rudnick-main/single.php
@@ -15,6 +15,9 @@ $data['featured_image_url'] = $data['featured_image_url'][0];
 $data['header_text'] = get_field('header_text');
 $parent = get_page($post->post_parent);
 $parent_name = $parent->post_name;
+$comments = get_comments($post); 
+$data['comments'] = $comments;
+
 
 ?>
 
@@ -26,38 +29,6 @@ $parent_name = $parent->post_name;
   <body>
     <div id="page-full-width-homepage" class ="full-width" role="main">
 		<?php Timber::render('/twig-templates/individual-blog-post.twig', $data); ?>
-    		<form action="<?php echo get_option( 'siteurl' ); ?>/wp-comments-post.php" method="post" id="commentform">
-		<p>
-			<label for="author">
-				<?php
-					_e( 'Name', 'foundationpress' ); if ( $req ) { _e( ' (required)', 'foundationpress' ); }
-				?>
-			</label>
-			<input type="text" class="five" name="author" id="author" value="<?php echo esc_attr( $comment_author ); ?>" size="22" tabindex="1" <?php if ( $req ) { echo "aria-required='true'"; } ?>>
-		</p>
-		<p>
-			<label for="email">
-				<?php
-					_e( 'Email (will not be published)', 'foundationpress' ); if ( $req ) { _e( ' (required)', 'foundationpress' ); }
-				?>
-			</label>
-			<input type="text" class="five" name="email" id="email" value="<?php echo esc_attr( $comment_author_email ); ?>" size="22" tabindex="2" <?php if ( $req ) { echo "aria-required='true'"; } ?>>
-		</p>
-		<p>
-			<label for="comment">
-					<?php
-						_e( 'Comment', 'foundationpress' );
-					?>
-			</label>
-			<textarea name="comment" id="comment" tabindex="4"></textarea>
-		</p>
-		<p><input name="submit" class="button" type="submit" id="submit" tabindex="5" value="<?php esc_attr_e( 'Submit', 'foundationpress' ); ?>"></p>
-		<?php comment_id_fields(); ?>
-		<?php do_action( 'comment_form', $post->ID ); ?>
-		<?php $comments = get_comments(); 
-			var_dump($comments);
-		?>
-	</form>
     </div>  
     <?php get_footer(); ?>
   </body>
