dc4ccfad4693489816f37e0e41269d327d8371eb
mondolabs <development@mondo.com>
2016-11-30 12:25:34 -0500

    ie support

--- wp-content/themes/brown-rudnick-main/header.php
+++ wp-content/themes/brown-rudnick-main/header.php
@@ -13,6 +13,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?> >
