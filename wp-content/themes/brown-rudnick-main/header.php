bc0b6f427b26599aa56eb94bb0b70a434bbafd12
mondolabs <development@mondo.com>
2016-11-30 16:44:42 -0500

    ie stylesheet

--- wp-content/themes/brown-rudnick-main/header.php
+++ wp-content/themes/brown-rudnick-main/header.php
@@ -14,8 +14,8 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/assets/stylesheets/ie.css" type="text/css" media="screen"/>
 		<!--[if IE]>
+			<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/assets/stylesheets/ie.css" type="text/css" media="screen"/>
 		<![endif]-->
 		<?php wp_head(); ?>
 	</head>
