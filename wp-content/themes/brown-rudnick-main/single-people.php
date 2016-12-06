e73a5358d2990aa9b5d000cd35940b23b844f816
Luis Castaneda <luiscastaneda@Luiss-MacBook-Pro.local>
2016-12-06 18:21:55 -0500

    changed guid for TimberPost LInk in industry, geography and practice template sidebars

--- wp-content/themes/brown-rudnick-main/single-people.php
+++ wp-content/themes/brown-rudnick-main/single-people.php
@@ -6,6 +6,7 @@ $data = Timber::get_context();
 $data['person'] = new TimberPost();
 $data['specialization'] = get_field('specialization');
 $data['related_experiences'] = get_field('related_experiences');
+
 $data['featured_image_url'] = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), $size = 'post-thumbnail' );
 $data['featured_image_url'] = $data['featured_image_url'][0];
 $card = get_field('v_card');
