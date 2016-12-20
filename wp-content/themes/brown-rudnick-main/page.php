3f6cde5de12536771dd5157fafec08530d89d4ee
mondolabs <development@mondo.com>
2016-12-20 17:13:36 -0500

    default page

--- wp-content/themes/brown-rudnick-main/page.php
+++ wp-content/themes/brown-rudnick-main/page.php
@@ -10,36 +10,19 @@
  * @since FoundationPress 1.0.0
  */
 
- get_header(); ?>
+$data = Timber::get_context();
+    
+get_header(); ?>
+<html>
+  <head>
+    <?php wp_head()?>
+  </head>
+  <body>
+    <div id="page-full-width-homepage" class ="full-width" role="main">
+      <?php Timber::render('/twig-templates/default.twig', $data); ?>
+    </div>
+    <?php do_action( 'foundationpress_after_content' ); ?>
+    <?php get_footer();?>
+  </body>
+</html>
 
- <?php get_template_part( 'template-parts/featured-image' ); ?>
-
- <div id="page" role="main">
-
- <?php do_action( 'foundationpress_before_content' ); ?>
- <?php while ( have_posts() ) : the_post(); ?>
-   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-       <header>
-           <h1 class="entry-title"><?php the_title(); ?></h1>
-       </header>
-       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-       <div class="entry-content">
-           <?php the_content(); ?>
-           <?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-       </div>
-       <footer>
-           <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-           <p><?php the_tags(); ?></p>
-       </footer>
-       <?php do_action( 'foundationpress_page_before_comments' ); ?>
-       <?php comments_template(); ?>
-       <?php do_action( 'foundationpress_page_after_comments' ); ?>
-   </article>
- <?php endwhile;?>
-
- <?php do_action( 'foundationpress_after_content' ); ?>
- <?php get_sidebar(); ?>
-
- </div>
-
- <?php get_footer();
