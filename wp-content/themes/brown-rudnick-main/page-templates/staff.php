1577b0b673451e61fd0fc05e6aff09a1113b14af
Luis Castaneda <luiscastaneda@Luiss-MacBook-Pro.local>
2016-12-21 10:36:25 -0500

    staff page benefits

--- wp-content/themes/brown-rudnick-main/page-templates/staff.php
+++ wp-content/themes/brown-rudnick-main/page-templates/staff.php
@@ -25,7 +25,7 @@ $data['features_header'] = get_field('features_header');
 $data['lawyers_features'] = get_field('lawyers_features');
 $data['features_bottom_text'] = get_field('features_bottom_text');
 $data['benefits_header'] = get_field('benefits_header');
-$data['lawyers_benefits'] = get_field('lawyers_benefits');
+$data['lawyers_benefits'] = get_field('benefits');
 $data['bottom_banner_text'] = get_field('bottom_banner_text');
 $data['bottom_banner_image'] = get_field('bottom_banner_image');
 
