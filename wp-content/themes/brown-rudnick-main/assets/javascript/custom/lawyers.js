47dc0a584414ca0c097dd351f6e8207f2eb124f5
mondolabs <development@mondo.com>
2016-12-21 19:02:28 -0500

    career landing pages

--- wp-content/themes/brown-rudnick-main/assets/javascript/custom/lawyers.js
+++ wp-content/themes/brown-rudnick-main/assets/javascript/custom/lawyers.js
@@ -3,13 +3,12 @@ var LOCATIONSEARCHER = {
 		// listen for changes in select options element
 		$('#locationSelect').change(function(event){
 			// build query string
+			console.log("selected");
 			var keyword = $('#locationSelect').val() || "";			
 			var queryStringBase = location.origin + location.pathname;
 			var queryString = "" + "?job_location_query="+keyword;
 			// reload the page with the keyword as the url
-			if ( $('body').hasClass('page-template-lawyers') || $('body').hasClass('page-template-staff') ){
-				window.location.replace(queryString);	
-			}
+			window.location.replace(queryString);	
 		});
 	},
 	// make select value reflect your most recent search param
@@ -19,11 +18,11 @@ var LOCATIONSEARCHER = {
 		if (jobKeyword !== undefined) {
 			locationSelect.val(decodeURIComponent(jobKeyword));
 		}
-		
 	}
 }
 
 $('document').ready(function(){
+	console.log("location searcher loaded");
 	LOCATIONSEARCHER.detectChange(); 
 	LOCATIONSEARCHER.onLoad();
 });
