1fbbaa6d426f587588a239949f3ffe324744d6e2
Luis Castaneda <luis.m.castaneda@gmail.com>
2016-12-24 17:47:25 -0500

    IE11 fixes

--- wp-content/themes/brown-rudnick-main/assets/javascript/custom/transitions.js
+++ wp-content/themes/brown-rudnick-main/assets/javascript/custom/transitions.js
@@ -1,23 +1,28 @@
 // smooth loading for pages
+var BR_ANIMATION = {
+    beginAnimations: function(){
+       $(".animsition").animsition({
+            inClass: 'fade-in',
+            outClass: 'fade-out',
+            inDuration: 500,
+            outDuration: 800,
+            loading: true,
+            loadingParentElement: 'body', //animsition wrapper element
+            loadingClass: 'animsition-loading',
+            timeout: false,
+            timeoutCountdown: 5000,
+            onLoadEvent: true,
+            browser: [ 'animation-duration', '-webkit-animation-duration'],
+            // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+            // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+            overlay : false,
+            overlayClass : 'animsition-overlay-slide',
+            overlayParentElement : 'body',
+            transition: function(url){ window.location.href = url; }
+        }); 
+    }
+};
 
 $(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'fade-in',
-    outClass: 'fade-out',
-    inDuration: 500,
-    outDuration: 800,
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
-  });
+  BR_ANIMATION.beginAnimations();
 });
